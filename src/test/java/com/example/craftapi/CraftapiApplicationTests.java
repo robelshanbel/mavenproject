0a6e532733483321189d67037d6db8d75ec06845
robelshanbel <52001231+robelshanbel@users.noreply.github.com>
2021-10-13 06:59:07 -0700

    Update CraftapiApplicationTests.java

--- src/test/java/com/example/craftapi/CraftapiApplicationTests.java
+++ src/test/java/com/example/craftapi/CraftapiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.craftapi;
 
+import com.example.craftapi.model.Candidates;
+import com.example.craftapi.repository.CandidateRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import java.util.*;
 @SpringBootTest
 class CraftapiApplicationTests {
 
+@Autowired
+    CandidateRepository candidateRepository;
     @Test
     void contextLoads() {
     }
 
+
+    @Test
+    void readAllCandidateTest(){
+        List<Candidates>  list=candidateRepository.findAll();
+Assertions.assertThat(list).size().isGreaterThan(0);
+    }
 }
